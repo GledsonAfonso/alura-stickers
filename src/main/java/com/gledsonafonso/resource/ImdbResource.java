800b2b9cad15cecaa3a76d20d42fd25b65827cdf
Gledson A <g_a6@live.com>
2022-07-20 02:45:40 -0300

    Fixing package name for resources

--- src/main/java/com/gledsonafonso/resource/ImdbResource.java
+++ src/main/java/com/gledsonafonso/resource/ImdbResource.java
@@ -1,4 +1,4 @@
-package com.gledsonafonso.controller;
+package com.gledsonafonso.resource;
 
 import java.util.List;
 
--- src/test/java/com/gledsonafonso/resource/ImdbResourceTest.java
+++ src/test/java/com/gledsonafonso/resource/ImdbResourceTest.java
@@ -1,4 +1,4 @@
-package com.gledsonafonso.controller;
+package com.gledsonafonso.resource;
 
 import org.junit.jupiter.api.Test;
 
